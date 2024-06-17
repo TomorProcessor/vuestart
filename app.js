199c88510988136405f98a8b505c926734a6d46d
Peter Tomordi <tomordi.peter@zengo.eu>
2024-06-17 13:47:17 +0200

    components

--- app.js
+++ app.js
@@ -126,4 +126,35 @@ const app = Vue.createApp({
         console.log('unmounted');
     }
 });
+
+app.component('friend-content', {
+   template: `
+        <li>
+          <h2>{{ friend.name }}</h2>
+          <button @click="toggleDetails">Show Details</button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{ friend.phone }}</li>
+            <li><strong>Email:</strong>{{ friend.email }}</li>
+          </ul>
+        </li>
+   `,
+    data() {
+       return {
+           detailsAreVisible: false,
+           friend: {
+               id: 1,
+               name: "John Doe",
+               phone: "+1234567890",
+               email: "john.doe@example.com"
+           }
+       }
+    },
+    methods: {
+       toggleDetails() {
+           this.detailsAreVisible = !this.detailsAreVisible
+       }
+    }
+
+});
+
 app.mount('#user-goal');
