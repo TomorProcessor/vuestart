ca1a7864cd771f74f0e5d5a569305269b109cf45
Peter Tomordi <tomordi.peter@zengo.eu>
2024-06-19 17:39:11 +0200

    dynamic route path

--- vuestart4/src/main.js
+++ vuestart4/src/main.js
@@ -4,6 +4,7 @@ import {createRouter, createWebHashHistory} from "vue-router";
 import App from './App.vue';
 import TeamsList from "@/components/teams/TeamsList.vue";
 import UsersList from "@/components/users/UsersList.vue";
+import TeamMembers from "@/components/teams/TeamMembers.vue";
 
 const router = createRouter({
     history: createWebHashHistory(),
@@ -13,6 +14,9 @@ const router = createRouter({
         },
         {
             path: '/users', component: UsersList
+        },
+        {
+            path: '/teams/:teamId', component: TeamMembers
         }
     ]
 });
