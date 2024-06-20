3544dfcdbd37ff6b479a2db2de5967368e13ab13
Peter Tomordi <tomordi.peter@zengo.eu>
2024-06-20 09:20:12 +0200

    route redirect, route alias, route catch

--- vuestart4/src/main.js
+++ vuestart4/src/main.js
@@ -1,22 +1,31 @@
 import { createApp } from 'vue';
-import {createRouter, createWebHashHistory} from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 
 import App from './App.vue';
 import TeamsList from "@/components/teams/TeamsList.vue";
 import UsersList from "@/components/users/UsersList.vue";
 import TeamMembers from "@/components/teams/TeamMembers.vue";
+import NotFound from "@/components/NotFound.vue";
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: [
         {
-            path: '/teams', component: TeamsList
+            //ilyenkor ténylegesen van url csere, beírják a /-t, akkor átvált /teams-re
+            path: '/', redirect: '/teams',
+        },
+        {
+            //ha itt alias van, akkor nincs url csere, sima /-nél is ez látszik
+            path: '/teams', component: TeamsList, /*alias: '/' */
         },
         {
             path: '/users', component: UsersList
         },
         {
             path: '/teams/:teamId', component: TeamMembers, props: true
+        },
+        {
+            path: '/:notFound(.*)', component: NotFound
         }
     ]
 });
