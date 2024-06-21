9f1d9c31cd4487143ce069d95d31aa1178799a07
Peter Tomordi <tomordi.peter@zengo.eu>
2024-06-21 09:01:19 +0200

    async components

--- vuestart4/src/main.js
+++ vuestart4/src/main.js
@@ -1,13 +1,14 @@
-import {createApp} from 'vue';
+import {createApp, defineAsyncComponent} from 'vue';
 import {createRouter, createWebHistory} from "vue-router";
 
 import App from './App.vue';
 import TeamsList from "@/components/teams/TeamsList.vue";
 import UsersList from "@/components/users/UsersList.vue";
 import TeamMembers from "@/components/teams/TeamMembers.vue";
-import NotFound from "@/components/NotFound.vue";
+// import NotFound from "@/components/NotFound.vue";
 import TestFooter from "@/components/TestFooter.vue";
 
+const NotFound = defineAsyncComponent(() => import('./components/NotFound.vue'));
 
 const router = createRouter({
     history: createWebHistory(),
@@ -18,11 +19,15 @@ const router = createRouter({
         },
         {
             //ha itt alias van, akkor nincs url csere, sima /-nél is ez látszik
-            name: 'teams', path: '/teams', components: {default: TeamsList, footer: TestFooter}, /*alias: '/' */ children: [
+            name: 'teams',
+            path: '/teams',
+            components: {default: TeamsList, footer: TestFooter}, /*alias: '/' */
+            children: [
                 {
                     name: 'team-members', path: ':teamId', component: TeamMembers, props: true
                 }
-            ]
+            ],
+            meta: {needsAuth: true}
         },
         {
             path: '/users', component: UsersList,
@@ -39,14 +44,21 @@ const router = createRouter({
          * a böngésző
          */
         if (savedPosition) return savedPosition;
-        return {left: 0, top: 0 };
+        return {left: 0, top: 0};
     }
 });
 router.beforeEach((to, from, next) => {
-    console.log(from,to);
-    next(); //ilyenkor nem gátol egy route-ot se
+    console.log(from, to);
+    // next(); //ilyenkor nem gátol egy route-ot se
     // next(false); ilyenkor egy route-ot se enged
     // next({name: 'team-members', params: {teamId: 't2'}}); ilyenkor fixen ide redirect-el
+    if (to.meta.needsAuth) {
+        //itt kellene csekkolni, hogy authentikálva van-e
+        console.log('auth kell');
+        next();
+    } else {
+        next();
+    }
 })
 const app = createApp(App)
 app.use(router)
